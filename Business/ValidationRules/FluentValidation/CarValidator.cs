8212fafbbe22ed562741fe7bfe79de18bbc93b56
mehmet19034 <mehmetbeken1903@gmail.com>
2021-09-14 23:24:18 +0300

    FluentValidation

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.ModelYear).GreaterThan(2000).WithMessage("Model Yılı 2000 veya daha yüksek olmalıdır");
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.BrandId).NotEmpty();
+            RuleFor(c => c.ColorId).NotEmpty();
+        }
+    }
+}
